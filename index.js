29d3d0b2661643740799e3ce6fb9a6680edf03d7
jlcarv1 <jorge@drogfarma.com.br>
2021-10-09 16:09:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <div>
+    <dl><dd><h1>ONBUS - COMO USAR</h1></dd></dl>
+    <dl><dd><p>24/06/2021</p></dd></dl><dl><dd><b>E</b>m nossas páginas poderá ser feito o acesso a uma busca pelos ônibus e pelos respectivos itinerários das várias linhas de ônibus que circulam pelo território brasileiro.</dd><dd><b>P</b>ara isso escolha o botão que o direcionará a página do estado no qual quer fazer a busca.</dd><dd><b>C</b>aso não esteja querendo fazer uma busca direta, bem abaixo da página, temos dois botões que permite continuar a percorrer as outras páginas do site, incluindo paginas de cidades de outros países além do Brasil.</dd></dl>
+    <dl><dd><h1>Ônibus no Rio de Janeiro - RJ</h1></dd></dl>
+    <dl><dd><b>E</b>xiste um novo modelo de negócio para resolver o problema dos ônibus na cidade do Rio de Janeiro, que a cada mês vê sumir linhas de ônibus tão necessárias para o deslocamento urbano da população.</dd><dd><b>S</b>egundo a secretária de transportes <b>Maína Celidônio</b>, em entrevista para o <b>jornal g1</b> das <b>Organizações Globo</b> disse, atualmente a receita das linhas passa pelas operadoras de cartão e é direcionada para as empresas operadoras.</dd><dd><b>M</b>as, ao que parece esse modelo não está funcionando, e então o que se propõe, é que a Prefeitura crie uma nova operadora, e receba a receita, repassando para as operadoras o custo de operação dessas linhas subtraídas da circulação. Permitindo a volta das mesmas.</dd></dl>
+    
+    <dl><dd><h1>MUNDO - ÔNIBUS URBANOS</h1></dd></dl>
+<dl><dd>Na maior parte do mundo, as pessoas usam o transporte público para se deslocar entre os bairros, vilas, municípios e até mesmo entre os estados ou condados.
+Nesses links abaixo, nós damos uma pequena visão dos meios de transportes terrestres utilizados em alguns países.
+Mas damos um ênfase nas empresas e linhas de ônibus, onde em alguns casos disponibilizamos em mapa, a provável rota percorrida por determinada linha.</dd></dl>
+<dl><dd>
+<a href="https://onbus.com.br/onbusDE.php" class="button" title="Alemanha">Deutschland</a>
+<a href="https://onbus.com.br/onbusAU.php" class="button" title="Austrália">Australia</a>
+<a href="https://onbus.com.br/onbusBG.php" class="button" title="Bulgária">България</a>
+<a href="https://onbus.com.br/onbusCL.php" class="button" title="Chile">Chile</a>
+<a href="https://onbus.com.br/onbusCN.php" class="button" title="China">中國</a>
+<a href="https://onbus.com.br/onbusEG.php" class="button" title="Egito">مصر</a>
+<a href="https://onbus.com.br/onbusUS.php" class="button" title="Estados Unidos">United State of America</a>
+<a href="https://onbus.com.br/onbusFR.php" class="button" title="França">France</a>
+<a href="https://onbus.com.br/onbusIT.php" class="button" title="Itália">Italia</a>
+<a href="https://onbus.com.br/onbusIE.php" class="button" title="Irlanda">Ireland</a>
+<a href="https://onbus.com.br/onbusJP.php" class="button" title="Japão">日本</a>
+<a href="https://onbus.com.br/onbusMX.php" class="button" title="México">México</a>
+<a href="https://onbus.com.br/onbusPY.php" class="button" title="Paraguai">Paraguay</a>
+<a href="https://onbus.com.br/onbusPT.php" class="button" title="Portugal">Portugal</a>
+<a href="https://onbus.com.br/onbusGB.php" class="button" title="Reino Unido">United Kingdom</a>
+<a href="https://onbus.com.br/onbusRU.php" class="button" title="Rússia">Россия</a>
+<a href="https://onbus.com.br/onbusCH.php" class="button" title="Suiça">Schweiz</a>
+<a href="https://onbus.com.br/onbusUA.php" class="button" title="Ucrânia">Україна</a>
+<a href="https://onbus.com.br/onbusUY.php" class="button" title="Uruguai">Uruguay</a>
+</dd></dl>
+    <dl><dd><h1>BRASIL - ÔNIBUS URBANOS</h1></dd></dl>
+<dl><dd>
+<a href="https://onbus.com.br/estados/acre.php" class="button" title="Acre">Acre</a>
+<a href="https://onbus.com.br/estados/alagoas.php" class="button" title="Alagoas">Alagoas</a>
+<a href="https://onbus.com.br/estados/amapa.php" class="button" title="Amapá">Amapá</a>
+<a href="https://onbus.com.br/estados/amazonas.php" class="button" title="Amazonas">Amazonas</a>
+<a href="https://onbus.com.br/estados/bahia.php" class="button" title="Bahia">Bahia</a>
+<a href="https://onbus.com.br/estados/ceara.php" class="button" title="Ceará">Ceará</a>
+<a href="https://onbus.com.br/estados/distritofederal.php" class="button" title="Distrito Federal">Distrito Federal</a>
+<a href="https://onbus.com.br/estados/espiritosanto.php" class="button" title="Espirito Santo">Espirito Santo</a>
+<a href="https://onbus.com.br/estados/goias.php" class="button" title="Goiás">Goiás</a>
+<a href="https://onbus.com.br/estados/maranhao.php" class="button" title="Maranhão">Maranhão</a>
+<a href="https://onbus.com.br/estados/matogrosso.php" class="button" title="Mato Grosso">Mato Grosso</a>
+<a href="https://onbus.com.br/estados/matogrossodosul.php" class="button" title="Mato Grosso do Sul">Mato Grosso do Sul</a>
+<a href="https://onbus.com.br/estados/minasgerais.php" class="button" title="Minas Gerais">Minas Gerais</a>
+<a href="https://onbus.com.br/estados/para.php" class="button" title="Pará">Pará</a>
+<a href="https://onbus.com.br/estados/paraiba.php" class="button" title="Paraíba">Paraíba</a>
+<a href="https://onbus.com.br/estados/parana.php" class="button" title="Paraná">Paraná</a>
+<a href="https://onbus.com.br/estados/pernambuco.php" class="button" title="Pernambuco">Pernambuco</a>
+<a href="https://onbus.com.br/estados/piaui.php" class="button" title="Piauí">Piauí</a>
+<a href="https://onbus.com.br/estados/riodejaneiro.php" class="button" title="Rio de Janeiro">Rio de Janeiro</a>
+<a href="https://onbus.com.br/estados/riograndedonorte.php" class="button" title="Rio Grande do Norte">Rio Grande do Norte</a>
+<a href="https://onbus.com.br/estados/riograndedosul.php" class="button" title="Rio Grande do Sul">Rio Grande do Sul</a>
+<a href="https://onbus.com.br/estados/rondonia.php" class="button" title="Rondônia">Rondônia</a>
+<a href="https://onbus.com.br/estados/roraima.php" class="button" title="Roraima">Roraima</a>
+<a href="https://onbus.com.br/estados/santacatarina.php" class="button" title="Santa Catarina">Santa Catarina</a>
+<a href="https://onbus.com.br/estados/saopaulo.php" class="button" title="São Paulo">São Paulo</a>
+<a href="https://onbus.com.br/estados/sergipe.php" class="button" title="Sergipe">Sergipe</a>
+<a href="https://onbus.com.br/estados/tocantins.php" class="button" title="Tocantins">Tocantins</a>
+</dd></dl>
+<dl><dd><h1>BRASIL - ÔNIBUS INTERESTADUAL</h1></dd></dl>
+<dl><dd><b>S</b>e você quiser, poderá encontrar no link abaixo, as linhas interestaduais do seu município usando esse novo link de busca.</dd></dl>
+<dl><dd><a href="https://onbus.com.br/aab.php" class="button2" title="encontre ônibus por Município">BUSCA POR MUNICIPIO</a></dd></dl>
+<dl><dd><b>A</b>cesse agora no link abaixo as mais de 20.000 linhas inseridas em nossa base de dados.</dd></dl>
+<dl><dd><a href="https://onbus.com.br/estadual.php" class="button2" title="Vejá os ônibus interestaduais">BUSCA POR ESTADO</a></dd></dl>
+</div>;
+ReactDOM.render(element, document.getElementById('root'));
